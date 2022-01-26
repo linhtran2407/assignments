130c8688d9888abd55bb15106a68ca9e346b50bc
linhtran <ltran1@brynmawr.edu>
2022-01-26 01:34:04 -0500

    done password.c

--- A01/password.c
+++ A01/password.c
@@ -1,3 +1,30 @@
-int main() {
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  char input[20];
+  printf("Enter a word: ");
+  scanf("%s", input);
+
+  int n = strlen(input);
+
+  for (int i = 0; i < n; i++)
+  {
+    if (input[i] == 'e')
+    {
+      input[i] = '3';
+    }
+    else if (input[i] == 'l')
+    {
+      input[i] = '1';
+    }
+    else if (input[i] == 'a')
+    {
+      input[i] = '@';
+    }
+  }
+
+  printf("Your bad password is %s\n", input);
   return 0;
 }
