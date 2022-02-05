ec6f056209b0d3fc29ca513edfbb452d37eb4388
linhtran <ltran1@brynmawr.edu>
2022-02-04 23:04:10 -0500

    assignment 2 done

--- A02/wordguess.c
+++ A02/wordguess.c
@@ -2,17 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <stdbool.h>
 
 void playGuessWord (char word[33]) {
   int len = strlen(word) - 1; // not count last char, which is either "/0" terminator or "\n"
 
   // store the word being filled up after each turn
-  char* currentWord = malloc(sizeof(char) * len + 1); 
-
-  if (currentWord == NULL) { 
-    printf("Cannot allocate memory. Exiting..."); 
-    exit(1); 
-  }
+  // allocate extra space for terminator
+  char currentWord[len + 1]; 
   
   // initialize the word with all '_'
   for (int i = 0; i < len; i++) {
@@ -32,52 +29,35 @@ void playGuessWord (char word[33]) {
     }
 
     printf("\n");
-
     char input = 0;
     printf("Guess a character: ");
     scanf(" %c", &input);
-
     printf("\n");
 
-    // store the indices at which the character matches the input
-    int matchedIndices[len]; 
-    int j = -1;
-
-    // find indices at which the character matches the chosen one
+    // find and replace if there's any match
+    bool found = false; 
     for (int i = 0; i < len; i++) {
       if (word[i] == input) {
-        j++;
-        matchedIndices[j] = i;
+        found = true;
+        if (currentWord[i] == '_') {
+          currentWord[i] = input;
+          guessedCount++;
+        }
       }
     }
 
-    // there is no match
-    if (j == -1) {
+    if (!found) {
       printf("Sorry, %c not found!\n\n", input);
-      continue;
-    }
-
-    for (int i = 0; i <= j; i++) {
-      int matchedIndex = matchedIndices[i];
-      if (currentWord[matchedIndex] == '_') {
-        // replace '_' by the input
-        currentWord[matchedIndex] = input;
-        guessedCount++;
-      }
     }
   }
 
   for (int i = 0; i < len; i++) {
     printf("%c ", currentWord[i]);
   }
-
   printf("\n");
-
   printf("You won in %d turns!\n", (turnCounter-1));
-
-  free(currentWord);
-  currentWord = NULL;
 }
+
 
 int main() {
   FILE* infile = NULL;
@@ -105,8 +85,6 @@ int main() {
   while (fgets(buffer, 33, infile) != NULL && index < wordIndex) {  
     index++; 
   }
-
-  printf("Chosen word is %s", buffer);
   
   // play the game!
   playGuessWord(buffer);
