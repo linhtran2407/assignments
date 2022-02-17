104a79b9f8ecc3b7b928764628a8db630d61c654
linhtran <ltran1@brynmawr.edu>
2022-02-16 22:32:13 -0500

    done assignment 4

--- A04/test_ppm.c
+++ A04/test_ppm.c
@@ -4,13 +4,8 @@
 #include "read_ppm.h"
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    printf("Invalid runtime arguments");
-    exit(1);
-  }
-
   int w, h;
-  struct ppm_pixel* pixels = read_ppm(argv[1], &w, &h);
+  struct ppm_pixel* pixels = read_ppm("feep-ascii.ppm", &w, &h);
 
   if (pixels == NULL) {
     printf("Error reading PPM file and creating 2d pixel array!");
@@ -18,7 +13,7 @@ int main(int argc, char* argv[]) {
   }
   
   // print struct ppm_pixel
-  printf("Testing file %s: %d %d\n", argv[1], w, h);
+  printf("Testing file feep-ascii.ppm: %d %d\n", w, h);
   for (int i = 0; i < h; i++) {
     for (int j = 0; j < w; j++) {
       struct ppm_pixel currPixel = pixels[i*w + j];
