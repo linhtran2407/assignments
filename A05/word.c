872ab1c5598aea6685052a4ab38ff1fa35385c3a
linhtran <ltran1@brynmawr.edu>
2022-02-23 10:49:04 -0500

    done task1

--- A05/word.c
+++ A05/word.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main() {
+  char chars[5];
+  printf("Enter 4 characters: \n");
+  scanf(" %s", chars);
+
+  unsigned int shiftedThirdChar = chars[2] << 8;
+  unsigned int shiftedSecondChar = chars[1] << 16;
+  unsigned int shiftedFirstChar = chars[0] << 24; 
+  printf("Debug: %c = 0x%08X\n", chars[3], chars[3]); // unchange
+  printf("Debug: %c = 0x%08X\n", chars[2], shiftedThirdChar);
+  printf("Debug: %c = 0x%08X\n", chars[1], shiftedSecondChar);
+  printf("Debug: %c = 0x%08X\n", chars[0], shiftedFirstChar);
+  unsigned int sum = (unsigned int) chars[3] + shiftedFirstChar + shiftedSecondChar + shiftedThirdChar;
+
+  printf("Your number is: %d (0x%08x)\n", sum, sum);
+
   return 0;
 }
