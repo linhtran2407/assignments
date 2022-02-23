91e071ad1df3c6863de23bbe806d087091e80f08
linhtran <ltran1@brynmawr.edu>
2022-02-23 12:25:08 -0500

    done task 2

--- A05/binarychar.c
+++ A05/binarychar.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <math.h>
+#include<stdlib.h>
 
 int main() {
+  char digits[9];
+  printf("Enter 8 bits: \n");
+  scanf(" %s", digits);
+
+  // calc decimal value of binary number
+  int exp = 7;
+  int decVal = 0;
+  for (int i = 0; i < 8; i++, exp--) {
+    if (digits[i] == '1') {
+      decVal += pow(2, exp);
+    }
+  }
+
+  printf("Your character is: %c\n", (char) decVal);
   return 0;
 }
