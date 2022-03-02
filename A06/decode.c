1dfa489eb0b947646e9b637a48d2a2aca3b364c6
linhtran <ltran1@brynmawr.edu>
2022-03-02 01:59:12 -0500

    done part 1

--- A06/decode.c
+++ A06/decode.c
@@ -1,7 +1,75 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "read_ppm.h"
 
+// divided the decoded bits into each block of 8 digits and convert from binary to ascii var
+void genResult (int index, int* decodedBits) {  
+  printf("Max number of characters in the image %d \n", (index /8));
+  char *result = malloc(index / 8 + 1);  
+  if (result == NULL) {
+    printf("Cannot allocate space for result\n");
+    exit(1);
+  }
+
+  int counter = 0, resIndex = 0;
+  while (counter < index) {
+    int decVal = 0;
+    for (int i = 0; i < 8; i++) {
+      if (decodedBits[counter] == 1) {
+        decVal += (1 << (7 - i));
+      }
+      counter++;
+    }
+    result[resIndex] = (char) decVal;
+    printf("%c", result[resIndex]);
+    resIndex++;
+  }
+
+  free(result);
+  result = NULL;
+}
+
 int main(int argc, char** argv) {
+  if (argc != 2) {
+    printf("Usage: %s\n", argv[0]);
+    exit(1);
+  }
+
+  int w, h;
+  struct ppm_pixel* pixels = read_ppm(argv[1], &w, &h);
+
+  if (pixels == NULL) {
+    printf("Error reading PPM file and creating 2d pixel array!");
+    exit(1);
+  }
+
+  int *decodedBits = malloc(sizeof(int) * (w) * (h) * 3);
+  if (decodedBits == NULL) {
+    printf("Cannot allocate space for decodedBits\n");
+    exit(1);
+  }
+
+  int index = 0; // count length of decodedBits
+  unsigned int mask = 0x1;
+
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      struct ppm_pixel currPixel = pixels[i*w + j];
+      for (int k = 0; k < 3; k++) {
+        decodedBits[index++] = mask & currPixel.colors[k];
+      }
+    }
+  }
+
+  printf("Reading %s with width %d and height %d\n", argv[1], w, h);
+  genResult(index, decodedBits);
+
+  free(pixels);
+  pixels = NULL;
+  free(decodedBits);
+  decodedBits = NULL;
+
   return 0;
 }
 
--- A06/read_ppm.c
+++ A06/read_ppm.c
@@ -1,14 +1,55 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "read_ppm.h"
 
-// TODO: Implement this function
-// Feel free to change the function signature if you prefer to implement an 
-// array of arrays
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  FILE* infile = NULL;
 
-  return NULL;
+  infile = fopen(filename, "rb");
+  if (infile == NULL) {
+    printf("Error: unable to open file");
+    return NULL;
+  }  
+  
+  char currentLine[300];
+  fgets(currentLine, 300, infile); // skip the line containing type of PPM
+  
+  // this line might be a comment or the dimension 
+  fgets(currentLine, 300, infile);
+  
+  if (currentLine[0] == '#') {
+      // skip the comment
+    fgets(currentLine, 300, infile);
+  }
+
+  // find index of the first digit of the height value
+  int indexFirstDigitHeight = 0;
+  for (int i = 0; i < strlen(currentLine); i++) {
+    if (isspace(currentLine[i]) != 0) {
+      indexFirstDigitHeight = i + 1;
+      break;
+    }
+  }
+  
+  *w = atoi(&currentLine[0]);
+  *h = atoi(&currentLine[indexFirstDigitHeight]);
+
+  // skip the line of maximum color value
+  fgets(currentLine, 300, infile);
+
+  struct ppm_pixel *pixels = malloc(sizeof(struct ppm_pixel) * (*w) * (*h));
+  if (pixels == NULL) {
+    printf("Cannot allocate space for pixels\n");
+    return NULL;
+  }
+
+  fread(pixels, sizeof(struct ppm_pixel), (*w) * (*h), infile); 
+  
+  fclose(infile);
+
+  return pixels;
 }
 
 // TODO: Implement this function
