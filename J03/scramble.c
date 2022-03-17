f159afe54aa55085ad0317454533a2a9bc8b5570
linhtran <ltran1@brynmawr.edu>
2022-03-17 15:36:55 -0400

    done j3

--- J03/scramble.c
+++ J03/scramble.c
@@ -1,3 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+// avoid using math.h because that requires
+// linking to math library when compile
+int calcPower (int base, int exp) {
+  int res = 1;
+  while (exp > 0) {
+    res *= base;
+    --exp;
+  }
+  
+  return res;
+}
+
+void char2bin (char c){
+	int i, bin;
+	char ch = c;
+  int second, third;
+  int bins[8];
+  int index = 0;
+
+	for (i = 0; i < 8; i++) {
+		bin = ((ch << i) & 0x80) ? 1 : 0;
+    if (i == 6) { second = bin; }
+    if (i == 5) { third = bin; }
+    bins[index++] = bin;
+	}
+  
+  // swap
+  bins[5] = second;
+  bins[6] = third;  
+
+  int dec = 0;
+  int exp = 7;
+  for (int i = 0; i < 8; i++, exp--) {
+    if (bins[i] == 1) {
+      dec += calcPower(2, exp);
+    }
+  }
+  printf("%c", (char) dec);
+}
+
 int main() {
-  return 0;
+	char msg[1024];
+  printf("Enter a word: \n");
+  scanf("%[^\n]s", msg);
+	char letter;
+	int n = 0;
+	
+	for (int i = 0; i < strlen(msg); i++) {
+		letter = msg[i];
+		char2bin(letter);
+		n++;
+		if (n == 14) {
+			n = 0;
+		}
+	}
+
+	printf("\n");
+	return(0);
 }
