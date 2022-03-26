35fa2cc7a59b3353eead0b31da654373c1f448d1
linhtran <ltran1@brynmawr.edu>
2022-03-26 15:44:41 -0400

    fix code

--- J03/scramble.c
+++ J03/scramble.c
@@ -1,57 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <math.h>
-
-// avoid using math.h because that requires
-// linking to math library when compile
-int calcPower (int base, int exp) {
-  int res = 1;
-  while (exp > 0) {
-    res *= base;
-    --exp;
-  }
-  
-  return res;
-}
-
-void char2bin (char c){
-	int i, bin;
-	char ch = c;
-  int second, third;
-  int bins[8];
-  int index = 0;
-
-	for (i = 0; i < 8; i++) {
-		bin = ((ch << i) & 0x80) ? 1 : 0;
-    if (i == 6) { second = bin; }
-    if (i == 5) { third = bin; }
-    bins[index++] = bin;
-	}
-  
-  // swap
-  bins[5] = second;
-  bins[6] = third;  
-
-  int dec = 0;
-  int exp = 7;
-  for (int i = 0; i < 8; i++, exp--) {
-    if (bins[i] == 1) {
-      dec += calcPower(2, exp);
-    }
-  }
-  printf("%c", (char) dec);
-}
 
 int main() {
 	char msg[1024];
   printf("Enter a phrase: \n");
   scanf("%[^\n]s", msg);
-	char letter;
+	
 	printf("Scramble: ");
 	for (int i = 0; i < strlen(msg); i++) {
-		letter = msg[i];
-		char2bin(letter);
+		unsigned int secondBit = msg[i] & 0b00000010;
+		unsigned int thirdBit = msg[i] & 0b00000100;
+    
+
+    if (secondBit == 0b00000000 && thirdBit == 0b00000100){
+      msg[i] = msg[i] | 0b00000010; // set second bit to 1
+      msg[i] = msg[i] & 0b11111011; // set third bit to 0
+    } else if (secondBit == 0b00000010 && thirdBit == 0b00000000){
+      msg[i] = msg[i] & 0b11111101; // set second bit to 0
+      msg[i] = msg[i] | 0b00000100; // set third bit to 1
+    }
+
+    printf("%c", msg[i]);
 	}
 
 	printf("\n");
