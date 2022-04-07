d663cb0a54f0d21f5e994bcac4ed1fb9407b5756
linhtran <ltran1@brynmawr.edu>
2022-04-07 19:34:12 -0400

    done code

--- A09/read_ppm.c
+++ A09/read_ppm.c
@@ -1,16 +1,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "read_ppm.h"
 
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
+  FILE* infile = NULL;
 
-  // your code here
-  return NULL;
+  infile = fopen(filename, "rb");
+  if (infile == NULL) {
+    printf("Error: unable to open file");
+    return NULL;
+  }  
+  
+  char currentLine[300];
+  fgets(currentLine, 300, infile); // skip the line containing type of PPM
+  
+  // this line might be a comment or the dimension 
+  fgets(currentLine, 300, infile);
+  
+  if (currentLine[0] == '#') {
+      // skip the comment
+    fgets(currentLine, 300, infile);
+  }
+
+  // find index of the first digit of the height value
+  int indexFirstDigitHeight = 0;
+  for (int i = 0; i < strlen(currentLine); i++) {
+    if (isspace(currentLine[i]) != 0) {
+      indexFirstDigitHeight = i + 1;
+      break;
+    }
+  }
+  
+  *w = atoi(&currentLine[0]);
+  *h = atoi(&currentLine[indexFirstDigitHeight]);
+
+  // skip the line of maximum color value
+  fgets(currentLine, 300, infile);
+
+  struct ppm_pixel *pixels = malloc(sizeof(struct ppm_pixel) * (*w) * (*h));
+  if (pixels == NULL) {
+    printf("Cannot allocate space for pixels\n");
+    return NULL;
+  }
+
+  fread(pixels, sizeof(struct ppm_pixel), (*w) * (*h), infile); 
+  
+  fclose(infile);
+
+  return pixels;
 }
 
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+  FILE* fp = fopen(filename, "wb");
+  if (fp == NULL) {
+    printf("Error: unable to open file");
+    return;
+  }
+  
+  printf("Writing file %s \n", filename);
+  // header
+  fprintf(fp, "P6\n# This is comment\n%d %d\n255\n", w, h);
 
-  // your code here
+  // write each color
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      struct ppm_pixel currPixel = pixels[i*w + j];
+      fwrite(currPixel.colors, 1, 3, fp);
+    }
+  }
+
+  fclose(fp); 
 }
 
--- A09/single_mandelbrot.c
+++ A09/single_mandelbrot.c
@@ -1,9 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <time.h>
 #include <sys/time.h>
 #include "read_ppm.h"
+
+struct ppm_pixel* genPalette (int maxIterations) {
+  struct ppm_pixel *pallet = malloc(sizeof(struct ppm_pixel) * maxIterations);
+  if (pallet == NULL) {
+    printf("Cannot allocate space for pallet\n");
+    return NULL;
+  }
+  
+  srand(time(0));
+  // create a palette with maxIterations colors
+  for (int i = 0; i < maxIterations; i++) {
+    pallet[i].red = rand() % 255;
+    pallet[i].green = rand() % 255;
+    pallet[i].blue = rand() % 255;  
+  }
+
+  return pallet;
+}
+
+struct ppm_pixel* computeImg (struct ppm_pixel* pallet, int size, float xmin, float xmax, float ymin, float ymax, int maxIterations) {
+  float xfrac, yfrac, x0, y0, x, y, xtmp;
+  int iter;
+  struct ppm_pixel color;
+  struct ppm_pixel* pixels = malloc (sizeof(struct ppm_pixel) * size * size);
+  if (pixels == NULL) {
+    printf("Cannot allocate space for pixels\n");
+    return NULL;
+  }
+
+  for (int row = 0; row < size; row++) {
+    for (int col = 0; col < size; col++) {
+      xfrac = (float) row / size;
+      yfrac = (float) col / size;
+      x0 = xmin + xfrac * (xmax - xmin);
+      y0 = ymin + yfrac * (ymax - ymin);
+
+      x = 0.0;
+      y = 0.0;
+      iter = 0;
+      while (iter < maxIterations && ((x*x + y*y) <= 2*2)) {
+        xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+        iter++;
+      }
+
+      if (iter < maxIterations) {// escaped
+        color = pallet[iter];
+      } else {
+        color.red = 0;
+        color.blue = 0;
+        color.green = 0;
+      }
+
+      pixels[row*size + col] = color;
+    }
+  }     
+    return pixels;
+}
+
+
 
 int main(int argc, char* argv[]) {
   int size = 480;
@@ -12,6 +74,9 @@ int main(int argc, char* argv[]) {
   float ymin = -1.12;
   float ymax = 1.12;
   int maxIterations = 1000;
+  double timer;
+  struct timeval tstart, tend;
+  time_t tvalue;
 
   int opt;
   while ((opt = getopt(argc, argv, ":s:l:r:t:b:")) != -1) {
@@ -28,10 +93,23 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your work here
   // generate pallet
-  srand(time(0));
+  struct ppm_pixel *pallet = genPalette(maxIterations);
 
   // compute image
+  gettimeofday(&tstart, NULL);
+  struct ppm_pixel *pixels = computeImg(pallet, size, xmin, xmax, ymin, ymax, maxIterations);
+  gettimeofday(&tend, NULL);
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+  printf("Computed mandelbrot set (%dx%d) in %g seconds\n", size, size, timer);
 
+  // create file name and write the image
+  char filename[128] = "";
+  snprintf(filename, 128, "mandelbrot-%d-%ld.ppm", size, time(0));
+  write_ppm(filename, pixels, size, size);
+
+  free(pallet);
+  pallet = NULL;
+  free(pixels);
+  pixels = NULL;
 }
